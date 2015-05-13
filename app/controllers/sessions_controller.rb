fddc349e38b9531554e28c7384983a5052cdc25d
Ross Hamilton <rosshamilton.dev@gmail.com>
2015-05-13 23:29:57 +0100

    added user log in sessions + encryption

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,12 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
-
     else
       flash.now[:danger] = 'Invalid email/password combination'
-    render 'new'
+      render 'new'
     end
   end
 
   def destroy
-
   end
 end
