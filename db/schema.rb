674f93c00ef1822ad607eb8a76c405f7ec8aaad9
Ross Hamilton <rosshamilton.dev@gmail.com>
2015-07-01 20:31:11 +0100

    added password reset base

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619152211) do
+ActiveRecord::Schema.define(version: 20150701145230) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20150619152211) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.string   "reset_sent_at"
+    t.string   "datetime"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
