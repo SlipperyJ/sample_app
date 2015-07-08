be01d75a3143fca1682af0cfc88735b830ac2be4
Ross Hamilton <rosshamilton.dev@gmail.com>
2015-07-08 18:59:34 +0100

    added password reset restrictions

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -21,11 +21,33 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
+    if @user.password_reset_expired?
+      flash[:danger] = "Your password reset request has expired"
+      redirect_to new_password_reset_url
+    elsif (params[:user][:password].blank? &&
+           params[:user][:password_confirmation].blank?)
+      flash.now[:danger] = "Password/confirmation can't be blank"
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = "Password has been reset"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
 
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
     def get_user
       @user = User.find_by(email: params[:email])
+      unless (@user && @user.activated? && @user.authenticated?
+             (:reset, params[:id]))
+        redirect_to root_url
+      end
     end
 end
